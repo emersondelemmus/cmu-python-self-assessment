3235aa7ae181d0dab542e3cf69b6984a9d29628b
Emerson de Lemmus <edelemmus@gmail.com>
2022-12-12 20:58:00 -0600

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -1,6 +1,8 @@
-## Which for-loop will print Output = 10?
+## Which for-loop(s) will print Output = 10?
 output = 0
 for i in range(1,10):
 for i in 1,2,3,4,5,6,7,8,9,0:
+for i in "1,2,3,4,5,6,7,8,9,0":
+for i in range(3,33,3)
     output += 1
 print(output)
